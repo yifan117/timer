97d01fd0fa3626688226d6c3115c75d931ff6f65
yifan117 <yifan.qiu17@gmail.com>
2022-09-11 13:52:20 +0930

    removed target from config

---
+++ .netlify/functions-internal/render.js
@@ -0,0 +1,29 @@
+const { init } = require('../serverless.js');
+
+exports.handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-fe0dac07.js","imports":["_app/immutable/start-fe0dac07.js","_app/immutable/chunks/index-4f5b2732.js","_app/immutable/chunks/singletons-71d100e3.js"],"stylesheets":[]},
+		nodes: [
+			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
